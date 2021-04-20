8886b588c7802124bfa568a2dcc95c22344ab848
WULIbinbin <920658022@qq.com>
2021-04-20 09:07:03 +0000

    Deploying to git-page from  @ c043c2c0acd39298c7122fc50f66392fa2f512e2 🚀

--- assets/index.65fbbbfc.js
+++
@@ -1,1 +0,0 @@
-import{f as e,g as l,d as o}from"./vendor.3e742bbb.js";var r=e({setup(){return l((()=>{console.log(this)})),()=>o("div",null,[o("h2",null,["路由例子"])])}});export default r;
---
+++ assets/index.85b6c243.js
@@ -0,0 +1,1 @@
+import{f as a,g as e,d as l}from"./vendor.3e742bbb.js";var r=a({setup:()=>(e((()=>{})),()=>l("div",null,[l("h2",null,["路由例子"])]))});export default r;
