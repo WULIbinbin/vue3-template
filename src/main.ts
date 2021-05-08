7cd264d43db906ea8a677334b52d0581690bd0f5
吴焯斌 <920658022@qq.com>
2021-05-09 01:13:22 +0800

    feat:fix

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import store from './store/index'
 import App from './App'
 
 import '@/assets/js/setRem.js'
-//全局组件注册
-import FormItem from '@/components/FormItem/index'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
-app.component('form-item',FormItem)
 app.mount('#app')
